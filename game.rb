46e29af5e21fade6d418bf095406af196b9e474d
Grey Keith <r.grey.keith@gmail.com>
2017-03-30 16:35:03 -0700

    Added #play_turn method

--- game.rb
+++ game.rb
@@ -1,10 +1,39 @@
+require_relative 'board.rb'
+
+
 class Minesweeper
   def initialize
     @board = Board.new
-    run
+    # run
+  end
+
+  def run
+    play_turn until game_over || won?
+  end
+
+  def play_turn
+    pos = ask_pos
+    if @board[pos].bomb
+      game_over
+    else
+      reveal(pos)
+    end
+  end
+
+  def reveal(pos)
+    @board[pos].revealed = true
+    @board.display
   end
 
 
+  def parse(string)
+    string.split(',').map { |el| Integer(el) }
+  end
+
+  def ask_pos
+    puts "Enter position to reveal"
+    parse(gets.chomp)
+  end
 
 
 end
